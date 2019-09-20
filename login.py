02fde1bdc1b30afa2146a0e9535c8e7bc1266e0c
siliconx <siliconx@163.com>
2019-09-20 23:18:49 +0800

    tabs to spaces

--- login.py
+++ login.py
@@ -1,20 +1,21 @@
 import requests
 from lxml import etree
 
-home = 'http://10.248.98.2/srun_portal_pc?ac_id=1&theme=basic2'
+home_url = 'http://10.248.98.2/srun_portal_pc?ac_id=1&theme=basic2'
+login_url = 'http://10.248.98.2/cgi-bin/get_challenge?callback=jQuery1124077608628830025_1568990931543&username=19S051022&ip=10.249.73.17&_=1568990931544'
 
 def ip():
-	'''获取ip地址.'''
-	# 下载页面
-	r = requests.get(home)
-	# 解析html
-	html = etree.HTML(r.text)
-	# 定位id为user_ip的<input>标签
-	ip_tag = html.xpath('//input[@id="user_ip"]')
-	if ip_tag:
-		# 提取ip地址
-		ip = ip_tag[0].attrib['value']
-	else:
-		ip = None
+    '''获取ip地址.'''
+    # 下载页面
+    r = requests.get(home_url)
+    # 解析html
+    html = etree.HTML(r.text)
+    # 定位id为user_ip的<input>标签
+    ip_tag = html.xpath('//input[@id="user_ip"]')
+    if ip_tag:
+        # 提取ip地址
+        ip = ip_tag[0].attrib['value']
+    else:
+        ip = None
 
-	return ip
+    return ip
